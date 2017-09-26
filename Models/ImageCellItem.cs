a13bcbd7180ffe37c5755d5b4b0514121cda8e8c
Ingvild <ingvild_kongsgaard@hotmail.com>
2017-09-26 11:46:29 -0700

    Error twitter.png not found

---
+++ Models/ImageCellItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ImageCellListView.Models
+{
+	public class ImageCellItem
+	{
+		public string ImageText
+		{
+			get;
+			set;
+		}
+
+		public ImageSource IconSource
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ MyPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ImageCellListView.Models;
+using Xamarin.Forms;
+
+namespace ImageCellListView
+{
+	public partial class MyPage : ContentPage
+	{
+        public MyPage()
+		{
+			InitializeComponent();
+
+			PopulateColorList();
+		}
+
+		private void PopulateColorList()
+		{
+			var imagesForListView = new ObservableCollection<ImageCellItem>()
+			{
+				new ImageCellItem()
+				{
+					IconSource = ImageSource.FromFile("twitter.png"),
+					ImageText = "Twitter",
+				},
+				new ImageCellItem()
+				{
+					IconSource = ImageSource.FromFile("google.png"),
+					ImageText = "Google",
+				},
+			};
+
+			ImageCells.ItemsSource = imagesForListView;
+		}
+	}
+}
